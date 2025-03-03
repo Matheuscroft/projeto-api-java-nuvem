bf5fac55339e131de715d491900d76498545ac64
Matheus OS <matheus.sdoliveira@gmail.com>
2025-03-02 22:18:56 -0300

    Initial commit

---
+++ src/main/java/com/matheus/projeto_api_java_nuvem/ProjetoApiJavaNuvemApplication.java
@@ -0,0 +1,13 @@
+package com.matheus.projeto_api_java_nuvem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoApiJavaNuvemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoApiJavaNuvemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheus/projeto_api_java_nuvem/ProjetoApiJavaNuvemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.projeto_api_java_nuvem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoApiJavaNuvemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
